ca1189856836947fbb685d7b9ea43801765162ce
Nicolas Muller <nmuller@debian.univ-jfc.fr>
2018-02-21 15:43:12 +0100

    titre

--- src/tuto_git/Application.java
+++ src/tuto_git/Application.java
@@ -21,6 +21,7 @@ public class Application extends Frame implements WindowListener {
 		
 		this.add(vue);
 		this.pack();
+		this.setTitle("Compteur");
 		this.setLocationRelativeTo(null);
 		this.setVisible(true);
 		this.addWindowListener(this);
