1fe1d5ad0573f337ab049d1e551280056be56946
Nicolas Muller <nmuller@debian.univ-jfc.fr>
2018-02-21 15:31:47 +0100

    ajout du windowlistener

--- src/tuto_git/Application.java
+++ src/tuto_git/Application.java
@@ -1,8 +1,10 @@
 package tuto_git;
 
 import java.awt.Frame;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
 
-public class Application extends Frame {
+public class Application extends Frame implements WindowListener {
 
 	public static void main(String[] args) {
 		new Application();
@@ -21,6 +23,49 @@ public class Application extends Frame {
 		this.pack();
 		this.setLocationRelativeTo(null);
 		this.setVisible(true);
+		this.addWindowListener(this);
+	}
+
+	@Override
+	public void windowActivated(WindowEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void windowClosed(WindowEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void windowClosing(WindowEvent e) {
+		// TODO Auto-generated method stub
+		System.exit(0);
+	}
+
+	@Override
+	public void windowDeactivated(WindowEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void windowDeiconified(WindowEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void windowIconified(WindowEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void windowOpened(WindowEvent e) {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
